9dc489e4bd9db4c3a47beaa44579e075689db447
Frank Denis <github@pureftpd.org>
2023-02-23 19:38:41 +0100

    Add Ascon to the benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -20,18 +20,27 @@ fn test_aes128gcm(m: &mut [u8]) {
     state.encrypt_in_place_detached(nonce, &[], m).unwrap();
 }
 
-fn test_aegis128l(m: &mut [u8]) {
-    let key = [0u8; 16];
-    let nonce = [0u8; 16];
-    let state = Aegis128L::new(&nonce, &key);
-    state.encrypt_in_place(m, &[]);
-}
-
 fn test_chacha20poly1305(m: &mut [u8]) {
     let key = chacha20poly1305::Key::from_slice(&[0u8; 32]);
     let nonce = chacha20poly1305::Nonce::from_slice(&[0u8; 12]);
     let state = ChaCha20Poly1305::new(key);
     state.encrypt_in_place_detached(nonce, &[], m).unwrap();
+}
+
+fn test_ascon128a(m: &mut [u8]) {
+    let key = [0u8; 16];
+    let nonce = [0u8; 16];
+    let state = ascon_aead::Ascon128a::new(key.as_slice().into());
+    state
+        .encrypt_in_place_detached(nonce.as_slice().into(), &[], m)
+        .unwrap();
+}
+
+fn test_aegis128l(m: &mut [u8]) {
+    let key = [0u8; 16];
+    let nonce = [0u8; 16];
+    let state = Aegis128L::new(&nonce, &key);
+    state.encrypt_in_place(m, &[]);
 }
 
 fn main() {
@@ -55,6 +64,9 @@ fn main() {
 
     let res = bench.run(options, || test_chacha20poly1305(&mut m));
     println!("chacha20-poly1305 : {}", res.throughput(m.len() as _));
+
+    let res = bench.run(options, || test_ascon128a(&mut m));
+    println!("ascon128a         : {}", res.throughput(m.len() as _));
 
     let res = bench.run(options, || test_aegis128l(&mut m));
     println!("aegis128l         : {}", res.throughput(m.len() as _));
