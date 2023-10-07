ea77b692edd10e5abd5f95efd192a161f77defcf
Frank Denis <github@pureftpd.org>
2023-10-07 14:39:11 +0200

    Unbreak the pure_rust version

--- src/pure_rust/aegis128l.rs
+++ src/pure_rust/aegis128l.rs
@@ -1,26 +1,3 @@
-#[cfg(not(any(
-    all(target_arch = "x86_64", target_feature = "aes"),
-    all(target_arch = "x86", target_feature = "aes")
-)))]
-mod aes_crate;
-#[cfg(not(any(
-    all(target_arch = "x86_64", target_feature = "aes"),
-    all(target_arch = "x86", target_feature = "aes")
-)))]
-use aes_crate::AesBlock;
-
-#[cfg(all(any(
-    all(target_arch = "x86_64", target_feature = "aes"),
-    all(target_arch = "x86", target_feature = "aes")
-)))]
-mod aes_ni;
-
-#[cfg(all(any(
-    all(target_arch = "x86_64", target_feature = "aes"),
-    all(target_arch = "x86", target_feature = "aes")
-)))]
-use aes_ni::AesBlock;
-
 use super::AesBlock;
 pub use crate::Error;
 
--- src/pure_rust/mod.rs
+++ src/pure_rust/mod.rs
@@ -1,1 +1,24 @@
+#[cfg(not(any(
+    all(target_arch = "x86_64", target_feature = "aes"),
+    all(target_arch = "x86", target_feature = "aes")
+)))]
+mod aes_crate;
+#[cfg(not(any(
+    all(target_arch = "x86_64", target_feature = "aes"),
+    all(target_arch = "x86", target_feature = "aes")
+)))]
+use aes_crate::AesBlock;
+
+#[cfg(all(any(
+    all(target_arch = "x86_64", target_feature = "aes"),
+    all(target_arch = "x86", target_feature = "aes")
+)))]
+mod aes_ni;
+
+#[cfg(all(any(
+    all(target_arch = "x86_64", target_feature = "aes"),
+    all(target_arch = "x86", target_feature = "aes")
+)))]
+use aes_ni::AesBlock;
+
 pub mod aegis128l;
